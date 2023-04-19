50543856350560bf6cc6765c2f9d72180c14c920
Raul <rmartiri@ull.edu.es>
2023-04-19 13:12:37 +0100

    setup.py fix 3

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@ from setuptools import setup, find_packages
 
 # ...
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r") as fh:
     README_DESCRIPTION = fh.read()
 
 # ...
 
 setup(
+    long_description=README_DESCRIPTION,
+    long_description_content_type="text/markdown",
     name='driveup',
     version='0.2.0',
     author='Raúl M.R.',
+    author_email="raul.martin4bc@gmail.com",
+    license="MIT",
     description='Python package for uploading files and folders to Google Drive',
-    long_description=README_DESCRIPTION,
-    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'pandas',
