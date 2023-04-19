40df9de7e35eb8a20eb2b355a9d9abef778e8d52
Raul <rmartiri@ull.edu.es>
2023-04-19 11:42:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Python package for uploading files and folders to Google Drive
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='driveup',
+    version='0.1.0',
+    author='Raúl M.R.',
+    description='Python package for uploading files and folders to Google Drive',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+)
