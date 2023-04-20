47e99a021cbe6ca4b0e127e8a05283b3fa4364a5
raul-martin-dev <raul.martin4bc@gmail.com>
2023-04-20 12:58:08 +0100

    setup.py fixed

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='driveup',
-    version='0.1.0',
+    version='0.2.0',
     author='Raúl M.R.',
     description='Python package for uploading files and folders to Google Drive',
+    long_description='Python package for uploading files and folders to Google Drive',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         'pandas',
