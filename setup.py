937e81a95f5b7ff2cac811ecd5024e89db776d8e
raul-martin-dev <raul.martin4bc@gmail.com>
2023-04-20 12:58:09 +0100

    setup.py fix 2: longdesc linked to README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
+# ...
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    README_DESCRIPTION = fh.read()
+
+# ...
+
 setup(
     name='driveup',
     version='0.2.0',
     author='Raúl M.R.',
     description='Python package for uploading files and folders to Google Drive',
-    long_description='Python package for uploading files and folders to Google Drive',
+    long_description=README_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
