66354f7c6daf4b38288e006f0bf1465dd012f547
raul-martin-dev <raul.martin4bc@gmail.com>
2023-04-28 08:09:42 +0100

    starting driveup new version

--- Driveup/features/auth.py
+++ Driveup/features/auth.py
@@ -4,12 +4,7 @@ from google.oauth2.service_account import Credentials
 class Auth:
     def authorize(self,client_secrets_file,service=False):
         scopes = ['https://www.googleapis.com/auth/drive']
-        if service == True:
-            creds = Credentials.from_service_account_file(client_secrets_file, scopes=scopes)
-            creds_type = 'service'
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes=scopes)
-            creds = flow.run_local_server(port=0)
-            creds_type = 'client'
+        flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes=scopes)
+        creds = flow.run_local_server(port=0)
 
-        return creds,creds_type
+        return creds
