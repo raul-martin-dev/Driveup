c97180153e3fbf009c87901feb909bca13bc4cb1
raul-martin-dev <raul.martin4bc@gmail.com>
2023-05-04 14:08:05 +0100

    auto-detect service/client in auth method (not tested)

--- Driveup/features/auth.py
+++ Driveup/features/auth.py
@@ -1,4 +1,5 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
+import json
 
 def authorize(client_secrets_file):
     scopes = ['https://www.googleapis.com/auth/drive']
@@ -6,3 +7,15 @@ def authorize(client_secrets_file):
     creds = flow.run_local_server(port=0)
 
     return creds
+
+def get_secret_type(path):
+
+    with open(path,'r') as f:
+        secret_dict = json.load(f)
+
+    if 'type' in secret_dict and secret_dict['type'] == 'service_account':
+        secret_type = 'service'
+    else:
+        secret_type = 'client'
+    
+    return secret_type
