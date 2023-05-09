cfdacb7044648d2190bbaab70679b52f21e783fe
raul-martin-dev <raul.martin4bc@gmail.com>
2023-05-09 13:16:10 +0100

    setting up fo v0.4.0

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ setup(
     long_description=README_DESCRIPTION,
     long_description_content_type="text/markdown",
     name='driveup',
-    version='0.3.0',
+    version='0.4.0',
     author='Raúl M.R.',
     author_email="raul.martin4bc@gmail.com",
     license="MIT",
     description='Python package for uploading files and folders to Google Drive',
     packages=find_packages(include=["Driveup","Driveup.features"]),
     install_requires=[
-        'pandas',
-        'pydrive',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
     ],
 )
