cc7d0f85852195872a28ebbf048ed737ab27f65d
Raúl Martín Rigor <rmartiri@GESTION.LOCAL>
2023-05-18 13:18:57 +0100

    utils mode added

--- Driveup/drive.py
+++ Driveup/drive.py
@@ -1,6 +1,7 @@
 from typing import overload,Union,List
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+from features import utils
 import os
 import re
 
@@ -45,10 +46,10 @@ class Drive:
                 folder_id = folder_id[0]
                 
             if url == True:
-                folder_id = self.url_to_id(folder_id)
+                folder_id = utils.url_to_id(folder_id)
 
             if file_title == None:
-                file_title = self.get_filename(file_path)
+                file_title = utils.get_filename(file_path)
 
             drive_service = self.drive_service
 
@@ -67,7 +68,7 @@ class Drive:
                     file_metadata = {'name': file_title,'parents': folder_id}
 
                 if convert == True:
-                    file_metadata = self.convert(file_metadata,self.get_file_extension(file_path))
+                    file_metadata = utils.convert(file_metadata,utils.get_file_extension(file_path))
 
                 gfile = drive_service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
@@ -81,7 +82,6 @@ class Drive:
                 file_id = file_metadata['id']
                 gfile = self.update(file_path,file_id)
                 
-
     def update(self,file_path: str,file_id: str):
         
         drive_service = self.drive_service
@@ -124,82 +124,19 @@ class Drive:
             sheets_service.spreadsheets().values().clear(spreadsheetId=id,range=sheet_name, body={}).execute()
             sheets_service.spreadsheets().values().batchUpdate(spreadsheetId=id, body=requests).execute()
 
-    
-    # returns metadata for the file (whether it exists or not)
-    def get_update(self,name,file_id,folder_id,service):
-        if file_id != None: # use specified id
-                if self.mode == 'client':
-                    file_metadata = {'id':file_id,'name': name,'parents': [folder_id]} # Change name: doesn't work
-                else:
-                    file_metadata = {'id':file_id,'name': name,'parents': folder_id} # Change name: doesn't work
-
-        else: # obtain id for duplicated file (file with same name) and overwrite
-            file_id = self.find_duplicate(self.list_files(folder_id,service),name = name)
-            if file_id != None: # duplicate found
-                if self.mode == 'client':
-                    file_metadata = {'id':file_id,'name': name,'parents': [folder_id]}
-                else:
-                    file_metadata = {'id':file_id,'name': name,'parents': folder_id}
-            else: # duplicate not found
-                file_metadata = None
-
-        return file_metadata
-            
-    def get_filename(self,path):
-        name = os.path.basename(path)
-        name = os.path.splitext(name)[0]
-
-        return name
-    
-    def get_file_extension(self,path):
-        name = os.path.basename(path)
-        extension = os.path.splitext(name)[1]
-        extension = extension.lower()
-        extension = extension.replace(".", "")
-
-        return extension
-    
-    def list_files(self,folder_id,service):
-        results = service.files().list(q=f"'{folder_id}' in parents and trashed = false", fields="nextPageToken, files(id, name)").execute()  
-        files = results.get('files', [])
-        
-        return files
-
-    def find_duplicate(self,list,name = None,file_id = None):
-        if name == None and file_id == None:
-            # needs to control error
-            pass
-
-        elif name != None: # name mode
-
-            for file in list:
-                if file['name'] == name:
-                    file_id = file['id']
-            
-            return file_id
-        
-        elif file_id != None: # id mode
-
-            condition = False
-            for file in list:
-                if file['id'] == file_id:
-                    condition = True
-            
-            return condition
-            
     def upload_folder(self,local_folder_path,folder_id,update=True,subfolder=True,subfolder_name=None,subfolder_id=None,recursive=True,convert=False,url=True):
 
         if url == True:
-            folder_id = self.url_to_id(folder_id)
+            folder_id = utils.url_to_id(folder_id)
 
         files_list = os.listdir(local_folder_path)
 
         if subfolder == True:
 
             if subfolder_name == None:
-                subfolder_name = self.get_filename(local_folder_path)
+                subfolder_name = utils.get_filename(local_folder_path)
 
-            folder_id = self.create_subfolder(subfolder_name,subfolder_id,folder_id,update)
+            folder_id = self.create_subfolder(subfolder_name,subfolder_id,folder_id,update,self.drive_service)
 
         for file in files_list:
             file_path = str(local_folder_path)+ '/' + file
@@ -213,64 +150,46 @@ class Drive:
                     print('\nError uploading file: ' + file_path + '\n(Not file or directory)')
             else:
                 self.upload(file_path,folder_id,update=update)
+    
+    # returns metadata for the file (whether it exists or not)
+    def get_update(self,name,file_id,folder_id,service):
+        if file_id != None: # use specified id
+                if self.mode == 'client':
+                    file_metadata = {'id':file_id,'name': name,'parents': [folder_id]} # Change name: doesn't work
+                else:
+                    file_metadata = {'id':file_id,'name': name,'parents': folder_id} # Change name: doesn't work
+
+        else: # obtain id for duplicated file (file with same name) and overwrite
+            file_id = utils.find_duplicate(self.list_files(folder_id,service),name = name)
+            if file_id != None: # duplicate found
+                if self.mode == 'client':
+                    file_metadata = {'id':file_id,'name': name,'parents': [folder_id]}
+                else:
+                    file_metadata = {'id':file_id,'name': name,'parents': folder_id}
+            else: # duplicate not found
+                file_metadata = None
+
+        return file_metadata
+    
+    def list_files(self,folder_id,service):
+        results = service.files().list(q=f"'{folder_id}' in parents and trashed = false", fields="nextPageToken, files(id, name)").execute()  
+        files = results.get('files', [])
+        
+        return files
             
-
-    def create_subfolder(self,subfolder_name,subfolder_id,parent_folder_id,update):
-
-        drive_service = self.drive_service
+    def create_subfolder(self,subfolder_name,subfolder_id,parent_folder_id,update,service):
 
         subfolder = None
 
         if update == True:
-            subfolder = self.get_update(subfolder_name,subfolder_id,parent_folder_id,drive_service)
+            subfolder = self.get_update(subfolder_name,subfolder_id,parent_folder_id,service)
         
         subfolder_metadata = {'name': subfolder_name, 'mimeType': 'application/vnd.google-apps.folder', 'parents': [parent_folder_id]}
 
         if subfolder == None:
-            subfolder = drive_service.files().create(body=subfolder_metadata, fields='id').execute()
+            subfolder = service.files().create(body=subfolder_metadata, fields='id').execute()
         else:
             subfolder['mimeType'] = subfolder_metadata['mimeType']
 
         return subfolder['id']
-    
-    # Transforms drive folder's URL to id. Returns ID if entry is not URL
-    def url_to_id(self,folder_id):
-        regex = r"(https?://)?(www\.)?(drive.google\.com/)?drive/folders/(\w+)"
-        match = re.search(regex, folder_id)
-        if match:
-            return match.group(4) # (\w+) -> Alphanumeric id
-        else:
-            return folder_id
-       
-    def convert(self,file_metadata,extension=None):
-        permited_general_extensions = ['txt',
-                                       'doc',
-                                       'odt',
-                                       'docm',
-                                       'csv',
-                                       'xlsx',
-                                       'xls',
-                                       'xlsm',
-                                       'fods',
-                                       'pptx']
-        
-        if extension == 'docx':
-            file_metadata['mimeType'] = 'application/vnd.google-apps.document'
-        elif extension == 'rtf':
-            file_metadata['mimeType'] = 'application/vnd.google-apps.document'
-        elif extension == 'md':
-            file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        elif extension == 'odf':
-            file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        elif extension == 'slk':
-            file_metadata['mimeType'] = 'application/vnd.google-apps.spreadsheet'
-        elif extension == 'prn':
-            file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        elif extension in permited_general_extensions :
-            file_metadata['mimeType'] = 'application/vnd.google-apps.' + extension
-        else:
-            file_metadata = file_metadata
-
-        
-        return file_metadata
     
---
+++ Driveup/features/utils.py
@@ -0,0 +1,80 @@
+import os
+
+def get_filename(path):
+    name = os.path.basename(path)
+    name = os.path.splitext(name)[0]
+
+    return name
+    
+def get_file_extension(path):
+    name = os.path.basename(path)
+    extension = os.path.splitext(name)[1]
+    extension = extension.lower()
+    extension = extension.replace(".", "")
+
+    return extension
+
+def find_duplicate(list,name = None,file_id = None):
+    if name == None and file_id == None:
+        # needs to control error
+        pass
+    elif name != None: # name mode
+
+        for file in list:
+            if file['name'] == name:
+                file_id = file['id']
+            
+        return file_id
+       
+    elif file_id != None: # id mode
+
+        condition = False
+        for file in list:
+            if file['id'] == file_id:
+                condition = True
+            
+        return condition
+    
+# Transforms drive folder's URL to id. Returns ID if entry is not URL
+def url_to_id(folder_id):
+    regex = r"(https?://)?(www\.)?(drive.google\.com/)?drive/folders/(\w+)"
+    match = re.search(regex, folder_id)
+    if match:
+        return match.group(4) # (\w+) -> Alphanumeric id
+    else:
+        return folder_id
+       
+def convert(file_metadata,extension=None):
+    permited_general_extensions = ['txt',
+                                    'doc',
+                                    'odt',
+                                    'docm',
+                                    'csv',
+                                    'xlsx',
+                                    'xls',
+                                    'xlsm',
+                                    'fods',
+                                    'pptx']
+        
+    if extension == 'docx':
+        file_metadata['mimeType'] = 'application/vnd.google-apps.document'
+    elif extension == 'rtf':
+        file_metadata['mimeType'] = 'application/vnd.google-apps.document'
+    elif extension == 'md':
+        file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    elif extension == 'odf':
+        file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    elif extension == 'slk':
+        file_metadata['mimeType'] = 'application/vnd.google-apps.spreadsheet'
+    elif extension == 'prn':
+        file_metadata['mimeType'] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    elif extension in permited_general_extensions :
+        file_metadata['mimeType'] = 'application/vnd.google-apps.' + extension
+    else:
+        file_metadata = file_metadata
+
+        
+    return file_metadata
+        
+        
+        
