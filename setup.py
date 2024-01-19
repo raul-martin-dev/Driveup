d6e895f920944e9aeb8f030385224f31756618f9
raul-martin-dev <raul.martin4bc@gmail.com>
2024-01-19 08:01:59 +0000

    setting up for 0.8.4 version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     long_description=README_DESCRIPTION,
     long_description_content_type="text/markdown",
     name='driveup',
-    version='0.8.3',
+    version='0.8.4',
     author='Raúl M.R.',
     author_email="raul.martin4bc@gmail.com",
     url="https://github.com/raul-martin-dev/Driveup",
