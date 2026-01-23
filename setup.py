403f56a95f5058cc0a183a38edd6568a8cd2421f
Raúl Martín <raul.martin4bc@gmail.com>
2026-01-23 17:45:44 +0000

    refactor: updated setup file

--- setup.py
+++ setup.py
@@ -1,26 +1,39 @@
 from setuptools import setup, find_packages
 
-# ...
-
 with open("README.md", "r") as fh:
     README_DESCRIPTION = fh.read()
 
-# ...
-
 setup(
-    long_description=README_DESCRIPTION,
-    long_description_content_type="text/markdown",
     name='driveup',
-    version='0.9.2',
+    version='0.9.3',
     author='Raúl M.R.',
     author_email="raul.martin4bc@gmail.com",
     url="https://github.com/raul-martin-dev/Driveup",
     license="MIT",
     description='Python package for uploading files and folders to Google Drive.',
-    packages=find_packages(include=["Driveup","Driveup.features"]),
+    long_description=README_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(include=["Driveup", "Driveup.*"]),
+    
     install_requires=[
-        'google-api-python-client',
-        'google-auth-httplib2',
-        'google-auth-oauthlib',
+        'google-api-python-client>=2.0.0',
+        'pandas>=1.0.0',
+        # 'google-auth-httplib2>=0.1.0',
+        'google-auth-oauthlib>=0.8.0',
     ],
+
+    extras_require={
+        'airflow': [
+            'apache-airflow>=2.4.0<3.0.0', 
+        ]
+    },
+
+    entry_points={
+        "apache_airflow_provider": [
+            "provider_info=Driveup.providers.airflow.get_provider_info:get_provider_info"
+        ]
+    },
+
+    python_requires='<3.12',
 )
